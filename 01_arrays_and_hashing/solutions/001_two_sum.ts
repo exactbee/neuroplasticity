b04d6b8f4ae460f054469e06e1b99b3244078e8a
Divyansh Bhatt <thankswe45@gmail.com>
2026-04-04 21:35:46 +0530

    solve: 001 two sum — O(n) hashmap solution

--- 01_arrays_and_hashing/solutions/001_two_sum.ts
+++ 01_arrays_and_hashing/solutions/001_two_sum.ts
@@ -29,6 +29,51 @@
 ***/
 
 function twoSum(nums: number[], target: number): number[] {
-  // TODO
+  /***
+   * Using double iteration and skipping self sum, repeated sum and number larger than target
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < nums.length; j++) {
+      if (nums[i] + nums[j] === target) {
+        return [i, j];
+      }
+    }
+  }
   return [];
+  ***/
+  /*
+   * Given target and inputs
+   * x + y = target
+   * Since target is known and if i iterate over the array one element will be known say x then
+   * y = target - x
+   * Now we need to calculate why but we shouldn't iterate the array again so we will use a data structure called hashmap
+   * hashmap gives us a way to find a element in just O(1) i.e  We will find a element in one ask without iterating .
+   */
+
+  const hashmap = new Map<number, number>();
+  for (let i = 0; i < nums.length; i++) {
+    const y = target - nums[i];
+    if (hashmap.has(y)) return [i, hashmap.get(y)!];
+    hashmap.set(nums[i], i);
+  }
+
+  /*
+   * The order of the code matters here since if i first put the element in hashmap and then calculate the value of y --
+   * Sometimes the target could be the double of a value, say target is 6 and array is [3,2,4] .
+   * Now if we  first pushed the 3 value in the hashmap and then calculate the y (6 - current element which is 3) = 3,
+   * it's the same number which is current element
+   */
+
+  return [];
+}
+
+const inputs = [
+  [2, 7, 11, 15],
+  [3, 2, 4],
+  [3, 4],
+  [3, 4, 5, 6, 7, 8],
+];
+const targets = [9, 6, 7, 11];
+
+for (let i = 0; i < inputs.length; i++) {
+  console.log(twoSum(inputs[i], targets[i]));
 }
